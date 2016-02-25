150293f43b5f42fa1bbc81b06d2b81be16e80aee
BalabanovGit <bzzz11111@google.com>
2016-02-25 20:42:00 +0300

    examples

---
+++ src/main/java/com/devsolutions/backend/example.java
@@ -0,0 +1,7 @@
+package com.devsolutions.backend;
+
+/**
+ * Created by vitmak38 on 2/25/16.
+ */
+public class example {
+}
---
+++ src/main/java/com/devsolutions/common/example.java
@@ -0,0 +1,7 @@
+package com.devsolutions.common;
+
+/**
+ * Created by vitmak38 on 2/25/16.
+ */
+public class example {
+}
